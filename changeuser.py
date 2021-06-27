827e0a7c1d092e19ec3981c4353684f32813b386
h-mirzaie <84665153+h-mirzaie@users.noreply.github.com>
2021-06-27 17:21:43 +0300

    Assign Phones To Users

--- changeuser.py
+++ changeuser.py
@@ -1,6 +1,6 @@
+from re import search
 from ciscoaxl import axl
 import yaml
-
 
 
 with open('info.yml', 'r') as stream:
@@ -14,22 +14,44 @@ ucm = axl(username,password,cucm,version)
 
 filter = {'tagfilter' :{'userid':'','uuid':'', 'telephoneNumber':''}}
 users = ucm.get_users(**filter)
-filter_IPPhone={'tagfilter' :{'name': ''}}
+filter_IPPhone = {'tagfilter' :{'name': ''}}
+Directory_list = []
+device=[]
+Associate_Devices={'device':[]}
 
-Directory_list=[]
 for p in ucm.get_phones(**filter_IPPhone):
     Phone_Name = (p.name)
     phone = (ucm.get_phone(name=Phone_Name))
     Phone_Lines = phone.lines
     Directory_Number = (Phone_Lines['line'][0]['dirn']['pattern'])
-    Directory_list.append(Directory_Number)
-
-for dn in Directory_list:
+    Associate_Devices={'device':[]}
+    
     for u in users:
         UserID = u.userid
         Telephone = (u.telephoneNumber)
-        if dn == Telephone:
-            print(ucm.update_user(userid=UserID, associatedDevices=Phone_Name))
+
+        if Directory_Number == Telephone:
+            user = ucm.get_user(UserID)
+            Associate_Devices=user.associatedDevices
+            print(UserID, Associate_Devices, Phone_Name)
+
+            if Associate_Devices is not None:
+
+                for i in  Associate_Devices:
+
+                    if Phone_Name in i :
+                        break
+
+                    else:
+                        Associate_Devices['device'].append(Phone_Name)
+
+            else:
+
+                Associate_Devices= Phone_Name
+                print('no', Associate_Devices)
+
+            print(ucm.update_user(**{'userid':UserID, 'associatedDevices' : Associate_Devices}))
+  
 
 
 
