d4f35706b2a5e79ea9e6d2cbdd16e9bac39f6938
h-mirzaie <84665153+h-mirzaie@users.noreply.github.com>
2021-06-28 14:30:44 +0300

    Remove Extra File

--- changeuser.py
+++ changeuser.py
@@ -1,4 +1,3 @@
-from re import search
 from ciscoaxl import axl
 import yaml
 
@@ -25,31 +24,21 @@ for p in ucm.get_phones(**filter_IPPhone):
     Phone_Lines = phone.lines
     Directory_Number = (Phone_Lines['line'][0]['dirn']['pattern'])
     Associate_Devices={'device':[]}
-    
     for u in users:
         UserID = u.userid
         Telephone = (u.telephoneNumber)
-
         if Directory_Number == Telephone:
             user = ucm.get_user(UserID)
             Associate_Devices=user.associatedDevices
             print(UserID, Associate_Devices, Phone_Name)
-
             if Associate_Devices is not None:
-
                 for i in  Associate_Devices:
-
                     if Phone_Name in i :
                         break
-
                     else:
                         Associate_Devices['device'].append(Phone_Name)
-
             else:
-
-                Associate_Devices= Phone_Name
-                print('no', Associate_Devices)
-
+                Associate_Devices= Phone_Name)
             print(ucm.update_user(**{'userid':UserID, 'associatedDevices' : Associate_Devices}))
   
 
